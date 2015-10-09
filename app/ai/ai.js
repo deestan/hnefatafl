3bcb59fc44cbe4161fc04d70791d9501de66135f
Helge Holm <helge.holm@gmail.com>
2015-10-09 17:15:58 +0200

    AI not broken anymore

--- app/ai/ai.js
+++ app/ai/ai.js
@@ -9,7 +9,7 @@ angular.module('myApp.ai', ['myApp.rules'])
       if (!(board.turn % 2) == !piece.black)
         validPieces.push({ index: pieceIndex });
     });
-    
+
     var moves = [];
     validPieces.forEach(function(piece, pieceIndex) {
       for (var row=0; row < 11; row++)
@@ -17,40 +17,52 @@ angular.module('myApp.ai', ['myApp.rules'])
           if (rules.validMove(board, piece.index, row, col))
             moves.push({pieceIndex: piece.index, row: row, col: col});
     });
-    
+
     return moves;
   }
 
-  function evaluateBoard(board, forBlack) {
+  function evaluateBoardForBlack(board) {
     var score = 0;
-    var goodForBlack = forBlack ? 1 : -1;
     if (rules.isWhiteWin(board))
-      score += -100 * goodForBlack;
+      score += -100;
     if (rules.isBlackWin(board))
-      score += 100 * goodForBlack;
+      score += 100;
     board.pieces.forEach(function(piece) {
       if (piece.dead) return;
       if (piece.black) {
-        score += goodForBlack;
+        score += 1;
       } else {
-        score -= goodForBlack;
+        score -= 1;
       }
     });
     return score;
   }
 
-  function search(depth, board, blackTurn) {
+  function search(depth, board, forBlack) {
     if (depth == 0) {
-      return evaluateBoard(board, !blackTurn);
+      var val = evaluateBoardForBlack(board);
+      return val;
     }
 
-    var moves = allMoves(board, blackTurn);
-    var bestMove, bestValue = -Infinity;
+    var bestValue = -Infinity;
+    var isBetterThan = function(blackScore, otherBlackScore) {
+      return blackScore > otherBlackScore;
+    }
+
+    if (!!(board.turn % 2) != !!forBlack) {
+      isBetterThan = function(blackScore, otherBlackScore) {
+        return blackScore < otherBlackScore;
+      }
+      bestValue = Infinity;
+    }
+
+    var moves = allMoves(board);
+    var bestMove;
     for (var i=0; i < moves.length; i++) {
       var move = moves[i];
       var newState = applyMove(board, move);
-      var moveValue = search(depth - 1, newState, !blackTurn);
-      if (moveValue > bestValue) {
+      var moveValue = search(depth - 1, newState, forBlack);
+      if (isBetterThan(moveValue, bestValue)) {
         bestMove = move;
         bestValue = moveValue;
       }
@@ -61,20 +73,23 @@ angular.module('myApp.ai', ['myApp.rules'])
   function applyMove(board, move) {
     var newBoard = {
       pieces: angular.copy(board.pieces),
-      turn: board.turn
+      turn: board.turn,
+      ended: board.ended
     };
     rules.applyMove(newBoard, move.pieceIndex, move.row, move.col);
     return newBoard;
   }
 
-  function bestMove(board) {
+  function findBestMove(board) {
+    var forBlack = !!board.turn;
     var moves = allMoves(board);
-    var bestMoves, bestValue = -Infinity;
+    var bestMoves = [], bestValue = -Infinity;
     for (var i=0; i < moves.length; i++) {
       var move = moves[i];
       var newState = applyMove(board, move);
-      var moveValue = search(1, newState, !!board.turn);
-      if (moveValue > bestValue) {
+      var moveValue = search(1, newState, forBlack);
+      if ((moveValue > bestValue && forBlack) ||
+          (moveValue < bestValue && !forBlack)) {
         bestMoves = [move];
         bestValue = moveValue;
       }
@@ -84,5 +99,5 @@ angular.module('myApp.ai', ['myApp.rules'])
     }
     return bestMoves[Math.random() * bestMoves.length >> 0];
   }
-  return { bestMove: bestMove };
+  return { bestMove: findBestMove };
 }]);
