96ea6bb20bb9517d4327d0964c1df69ae1804eaa
Helge Holm <helge.holm@gmail.com>
2015-10-14 16:18:40 +0200

    Remove CORS, since we're proxying from frontend now.

--- src/main/java/no/bouvet/hnefatafl/App.java
+++ src/main/java/no/bouvet/hnefatafl/App.java
@@ -6,11 +6,6 @@ import io.dropwizard.setup.Environment;
 import no.bouvet.hnefatafl.config.Config;
 import no.bouvet.hnefatafl.health.BrainHealthCheck;
 import no.bouvet.hnefatafl.resources.Brain;
-import org.eclipse.jetty.servlets.CrossOriginFilter;
-
-import javax.servlet.DispatcherType;
-import javax.servlet.FilterRegistration;
-import java.util.EnumSet;
 
 public class App extends Application<Config> {
     public static void main(String[] args) throws Exception {
@@ -29,16 +24,6 @@ public class App extends Application<Config> {
 
     @Override
     public void run(Config config, Environment environment) {
-        // Enable CORS headers
-        final FilterRegistration.Dynamic cors =
-                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
-        // Configure CORS parameters
-        cors.setInitParameter("allowedOrigins", "*");
-        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
-        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
-        // Add URL mapping for CORS
-        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
-
         Brain brain = new Brain(config.getSearchDepth());
         environment.jersey().register(brain);
 
