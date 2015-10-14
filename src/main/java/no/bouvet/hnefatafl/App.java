1914f46062d65fd90b36f58e051d6c1ae15bf35b
Helge Holm <helge.holm@gmail.com>
2015-10-14 14:38:24 +0200

    Fixed health check and corrected some indentation.

--- src/main/java/no/bouvet/hnefatafl/App.java
+++ src/main/java/no/bouvet/hnefatafl/App.java
@@ -5,7 +5,7 @@ import io.dropwizard.setup.Environment;
 import io.dropwizard.setup.Bootstrap;
 import no.bouvet.hnefatafl.config.Config;
 import no.bouvet.hnefatafl.resources.Brain;
-import no.bouvet.hnefatafl.health.TemplateHealthCheck;
+import no.bouvet.hnefatafl.health.BrainHealthCheck;
 
 import org.eclipse.jetty.servlets.CrossOriginFilter;
 import javax.servlet.DispatcherType;
@@ -29,20 +29,20 @@ public class App extends Application<Config> {
 
     @Override
     public void run(Config config, Environment environment) {
-		// Enable CORS headers
-		final FilterRegistration.Dynamic cors =
-			environment.servlets().addFilter("CORS", CrossOriginFilter.class);
-		// Configure CORS parameters
-		cors.setInitParameter("allowedOrigins", "*");
-		cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
-		cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
-		// Add URL mapping
-		cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+        // Enable CORS headers
+        final FilterRegistration.Dynamic cors =
+            environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
 
-		environment.jersey().register(new Brain(config.getSearchDepth()));
+        Brain brain = new Brain(config.getSearchDepth());
+        environment.jersey().register(brain);
 
-		final TemplateHealthCheck healthCheck =
-			new TemplateHealthCheck(config.getTemplate());
-		environment.healthChecks().register("template", healthCheck);
+        final BrainHealthCheck healthCheck = new BrainHealthCheck(brain);
+        environment.healthChecks().register("template", healthCheck);
     }
 }
---
+++ src/main/java/no/bouvet/hnefatafl/health/BrainHealthCheck.java
@@ -0,0 +1,28 @@
+package no.bouvet.hnefatafl.health;
+
+import com.codahale.metrics.health.HealthCheck;
+import no.bouvet.hnefatafl.core.Board;
+import no.bouvet.hnefatafl.core.Piece;
+import no.bouvet.hnefatafl.resources.Brain;
+
+import java.util.ArrayList;
+
+public class BrainHealthCheck extends HealthCheck {
+	private final Brain brain;
+
+	public BrainHealthCheck(Brain brain) {
+		this.brain = brain;
+	}
+
+	@Override
+	protected Result check() throws Exception {
+        Board emptyBoard = new Board();
+        emptyBoard.setPieces(new ArrayList<Piece>());
+        String outputJson = brain.PonderThis(emptyBoard);
+
+		if (outputJson != "\"null\"") {
+			return Result.unhealthy("Brain does not yield output.");
+		}
+		return Result.healthy();
+	}
+}
